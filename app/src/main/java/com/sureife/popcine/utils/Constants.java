cef2b3617ebc8cf0d0c611194a737d8e4f2a5de5
TheAnomaly <sureife@gmail.com>
2017-04-22 23:46:59 +0100

    add movie db image base url

---
+++ app/src/main/java/com/sureife/popcine/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.sureife.popcine.utils;
+
+/**
+ * Created by sureife on 16/04/2017.
+ */
+
+public class Constants {
+    public static final String MOVIE_DB_IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
+}
